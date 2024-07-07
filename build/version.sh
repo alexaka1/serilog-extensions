5f505c504a9ddf1c8e97e52c28b07192a9d735d2
Martossy Alex <github@mail.martossy.hu>
2024-07-07 12:45:34 +0200

    Extract to build script

---
+++ build/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PACKAGE_JSON_PATH=$1
+CSPROJ_PATH=$2
+PACKAGE_NAME=$2
+
+set -e
+yarn run version
+VERSION=$(jq -r '.version' "$PACKAGE_JSON_PATH")
+sed -i "s#<VersionPrefix>.*</VersionPrefix>#<VersionPrefix>$VERSION</VersionPrefix>#" "$CSPROJ_PATH"
+AVAILABLE_VERSIONS=$(curl -s "https://api.nuget.org/v3-flatcontainer/$PACKAGE_NAME/index.json" | jq -r '.versions[]')
+if echo "$AVAILABLE_VERSIONS" | grep -q "^$VERSION$"; then
+  echo "versionExists=true" >> "$GITHUB_OUTPUT"
+else
+  echo "versionExists=false" >> "$GITHUB_OUTPUT"
+fi
